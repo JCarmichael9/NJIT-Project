704e3725f12bbcc909840ab6db724ee9bb19f279
JCarmichael9 <526jcarmichael@frhsd.com>
2025-11-10 14:52:10 -0500

    fxed url

--- gallery.js
+++ gallery.js
@@ -1,52 +1,52 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'https://your-json-url.com' // Replace with actual JSON URL
+const mUrl = 'images.json' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
-  $('.details').hide() 
-startTimer();
-$('.moreIndicator').click(() => {
-  $('.moreIndicator').toggleClass('rot90 rot270')
-  $('.details').slideToggle()
-})
- 
-$('#nextPhoto').click(() => {
-  showNextPhoto();
-})
-$('#prevPhoto').click(() => {
-  showPrevPhoto();
-})
+  $('.details').hide()
+  startTimer();
+  $('.moreIndicator').click(() => {
+    $('.moreIndicator').toggleClass('rot90 rot270')
+    $('.details').slideToggle()
+  })
+
+  $('#nextPhoto').click(() => {
+    showNextPhoto();
+  })
+  $('#prevPhoto').click(() => {
+    showPrevPhoto();
+  })
 
   fetchJSON()
 })
 
 // Function to fetch JSON data and store it in mImages
-function fetchJSON () {
- $.ajax({
-  url: mUrl,
-  dataType: 'json',
-  success: function (data) {
-    mImages = data.images;
-    swapPhoto()
-  }, 
-  error: function (xhr, status, error) {
-    console.error("Failed to load")
-  }
- })
+function fetchJSON() {
+  $.ajax({
+    url: mUrl,
+    dataType: 'json',
+    success: function (data) {
+      mImages = data.images;
+      swapPhoto()
+    },
+    error: function (xhr, status, error) {
+      console.error("Failed to load")
+    }
+  })
 }
 
 // Function to swap and display the next photo in the slideshow
-function swapPhoto () {
-let currentImage = mImages[mCurrentIndex];
-$('#photo').attr('src', currentImage.imgPath);
-$('.location').text(`Location: ${currentImage.imgLocation}`)
-$('.description').text(`Description: ${currentImage.imgDescription}`)
-$('.date').text(`Date: ${currentImage.imgDate}`)
+function swapPhoto() {
+  let currentImage = mImages[mCurrentIndex];
+  $('#photo').attr('src', currentImage.imgPath);
+  $('.location').text(`Location: ${currentImage.imgLocation}`)
+  $('.description').text(`Description: ${currentImage.imgDescription}`)
+  $('.date').text(`Date: ${currentImage.imgDate}`)
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
-function showNextPhoto () {
+function showNextPhoto() {
   mCurrentIndex++;
   if (mCurrentIndex >= mImages.length) {
     mCurrentIndex = 0
@@ -55,7 +55,7 @@ function showNextPhoto () {
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
-function showPrevPhoto () {
+function showPrevPhoto() {
   mCurrentIndex--;
   if (mCurrentIndex < 0) {
     mCurrentIndex = mImages.length - 1
@@ -65,11 +65,11 @@ function showPrevPhoto () {
 
 let mTimer;
 // timer after 5 seconds chnages to next one
-function startTimer () {
- if (mTimer) {
-  clearInterval(mTimer);
- } 
- mTimer = setInterval(() => {
-  showNextPhoto();
- }, mWaitTime)
+function startTimer() {
+  if (mTimer) {
+    clearInterval(mTimer);
+  }
+  mTimer = setInterval(() => {
+    showNextPhoto();
+  }, mWaitTime)
 }
